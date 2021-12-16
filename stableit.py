f5d6ddd613bbc335f08e048ba992d8c03d6fc8ed
Kyle Aoki <69960581+kyle-aoki@users.noreply.github.com>
2021-12-16 12:18:26 -0800

    improved stability

--- stableit.py
+++ stableit.py
@@ -1,35 +1,77 @@
 import pyautogui
 import time
-from pynput import mouse
 from pynput import keyboard
+from threading import Thread
 
-listening = False
+
 last_action = time.time()
-pyautogui.FAILSAFE = False
+thread_delay = 29
+listener_min_start_time = 60 * 2
+keyboard_listener = None
+max_inaction_time = 60 * 5
+listening = False
 
-def record_event(*args):
-    global listening
+
+def handle_status_thread():
     global last_action
+    global thread_delay
+    global listener_min_start_time
+    global max_inaction_time
+    global listening
+    while True:
+        time.sleep(thread_delay)
+        print(f" --> Listening: {listening}, Time Elapsed: {round(time_elapsed())} ")
+
+
+def time_elapsed():
+    global last_action
+    return time.time() - last_action
+
+
+def handle_key_pressed(key):
+    global last_action
+    global listening
     last_action = time.time()
-    print('Stopping listener')
-    ml.stop()
-    kl.stop()
+    keyboard_listener.stop()
     listening = False
 
 
-print('program started')
+def start_keyboard_listener():
+    global keyboard_listener
+    global listening
+    keyboard_listener = keyboard.Listener(on_press=handle_key_pressed)
+    keyboard_listener.start()
+    listening = True
 
-while True:
-    if not listening:
-        time.sleep(30)
-        print('Starting listener')
-        listening = True
-        ml = mouse.Listener(on_move=record_event, on_click=record_event, on_scroll=record_event)
-        kl = keyboard.Listener(on_press=record_event)
-        ml.start()
-        kl.start()
-    while listening:
-        elapsed = time.time() - last_action
-        print(str(round(elapsed, 0)) + ' seconds elapsed')
-        if elapsed > 5: pyautogui.press('ctrl')
-        time.sleep(30)
+
+def handle_listener_thread():
+    global thread_delay
+    global listener_min_start_time
+    global listening
+    while True:
+        time.sleep(thread_delay)
+        if time_elapsed() > listener_min_start_time and not listening:
+            start_keyboard_listener()
+
+
+def wake_press():
+    global last_action
+    pyautogui.press('command')
+    last_action = time.time()
+
+
+def execute_thread_handler():
+    global max_inaction_time
+    while True:
+        time.sleep(thread_delay)
+        if time_elapsed() > max_inaction_time:
+            wake_press()
+            
+
+listener_thread = Thread(target=handle_listener_thread)
+executor_thread = Thread(target=execute_thread_handler)
+status_thread = Thread(target=handle_status_thread)
+
+listener_thread.start()
+executor_thread.start()
+status_thread.start()
