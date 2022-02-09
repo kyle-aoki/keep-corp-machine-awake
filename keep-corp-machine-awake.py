d9989b4cdad214921dd2b5c06504d8d72519d62d
Kyle Aoki <69960581+kyle-aoki@users.noreply.github.com>
2022-02-09 11:13:06 -0800

    Update keep-corp-machine-awake.py

--- keep-corp-machine-awake.py
+++ keep-corp-machine-awake.py
@@ -4,6 +4,7 @@ from pynput import keyboard
 from threading import Thread
 import os
 
+
 class KeepCorpMachineAwake:
 
     program_start_time = time.time()
@@ -11,18 +12,18 @@ class KeepCorpMachineAwake:
     thread_delay = 30
     listener_min_start_time = 60 * 2
     keyboard_listener = None
-    max_inaction_time = 60 * 5
+    max_inaction_time = 60 * 3
     listening = False
 
     def handle_status_thread(self):
         print("Program started.")
         print(f"Will update below information every {self.thread_delay} seconds.")
+
         while True:
             running_for = round(time.time() - self.program_start_time)
             listener_will_start = max(self.listener_min_start_time - self.time_elapsed(), 0)
             will_trigger_key_press_in = max(self.max_inaction_time - self.time_elapsed(), 0)
 
-            
             print("")
             print(f"  Running for:                   {running_for}")
             print(f"  Time since last key press:     {self.time_elapsed()}")
@@ -34,36 +35,30 @@ class KeepCorpMachineAwake:
             time.sleep(self.thread_delay)
             os.system('cls' if os.name == 'nt' else 'clear')
 
-    def time_elapsed(self):
-        return round(time.time() - self.last_action)
+    def time_elapsed(self): return round(time.time() - self.last_action)
 
     def handle_key_pressed(self, key):
+        if not self.listening: return
         print(f"Key '{key} pressed.")
         self.last_action = time.time()
-        self.keyboard_listener.stop()
         self.listening = False
 
-    def start_keyboard_listener(self):
+    def handle_listener_thread(self):
         self.keyboard_listener = keyboard.Listener(on_press=self.handle_key_pressed)
         self.keyboard_listener.start()
-        self.listening = True
 
-    def handle_listener_thread(self):
         while True:
             time.sleep(self.thread_delay)
             if self.time_elapsed() >= self.listener_min_start_time and not self.listening:
-                self.start_keyboard_listener()
+                self.listening = True
 
-    def wake_press(self):
-        pyautogui.press('ctrl')
-        self.last_action = time.time()
+    def wake_press(self): pyautogui.press('ctrl')
 
     def handle_execute_thread(self):
         while True:
             time.sleep(self.thread_delay)
             if self.time_elapsed() >= self.max_inaction_time:
                 self.wake_press()
-             
 
     def start(self):
         listener_thread = Thread(target=self.handle_listener_thread)
@@ -76,4 +71,5 @@ class KeepCorpMachineAwake:
 
 
 if __name__ == '__main__':
-    KeepCorpMachineAwake().start()
+    main = Thread(target=KeepCorpMachineAwake().start)
+    main.start()
